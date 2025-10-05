9718469b96e90c3aff8fb0c0136b0eb4769ab6b5
NhatMinhPhan <nhatminh120767@gmail.com>
2025-10-05 01:51:04 -0400

    Almost completed Home Page (just needed crew descriptions and animated GIFs)

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,6 +4,9 @@ import calculusImage from "./images/calculus.png";
 import sessionImage from "./images/session.png";
 import placeholderPFP from "./images/placeholder-pfp.png";
 import logoImage from "./images/hack-the-valley-echo-draw.png";
+import georgePFP from "./images/linkedin/george.jpg";
+import minhPFP from "./images/linkedin/minh.jpg";
+import kaleelPFP from "./images/linkedin/kaleel.jpg";
 
 export default function Home() {
   return (
@@ -23,29 +26,49 @@ export default function Home() {
             <div className="flex-1 flex flex-row gap-x-10 bg-[#34a8a2] px-8 py-5 rounded-[15px] homepage-description-section">
               <img
                 src={lectureImage.src}
-                style={{ width: "50%", borderRadius: "25px" }}
+                style={{
+                  width: "50%",
+                  objectFit: "contain",
+                  borderRadius: "25px",
+                }}
               />
               <HomePageDescription
-                question="What is this?"
-                text="Powered by Next.js, Supabase and the Excalidraw API, we help make learning more engaging by enabling students to get creative!"
+                question="What's EchoDraw?"
+                text="As students, we’ve all experienced classrooms where engagement fades quickly — students hesitate to participate for fear of making mistakes, and teachers struggle to identify learning gaps in real time. We were inspired to create EchoDraw to make classrooms more interactive, anonymous, and insightful. By blending collaborative whiteboarding with real-time feedback, EchoDraw helps teachers see what students are thinking without the pressure of raising hands."
               />
             </div>
             <div className="flex-1 flex flex-row gap-x-10 bg-[#007b80] px-8 py-5 rounded-[15px] homepage-description-section">
               <HomePageDescription
                 question="How does it work?"
-                text="Professors create a session which is presented in the classroom. Students will join the session with just a PIN!"
+                text={`Powered by Next.js, TailwindCSS, Supabase and the Excalidraw API, EchoDraw allows teachers to create lessons composed of digital whiteboard questions.
+Students can join a lesson using a unique code, respond on their own digital whiteboards, and submit their answers anonymously.`}
               />
               <img
                 src={sessionImage.src}
-                style={{ width: "50%", borderRadius: "25px" }}
+                style={{
+                  width: "50%",
+                  objectFit: "contain",
+                  borderRadius: "25px",
+                }}
               />
             </div>
             <div className="flex-1 flex flex-row gap-x-10 bg-[#34a8a2] px-8 py-5 rounded-[15px] homepage-description-section">
               <img
                 src={calculusImage.src}
-                style={{ width: "50%", borderRadius: "25px" }}
+                style={{
+                  width: "50%",
+                  objectFit: "contain",
+                  borderRadius: "25px",
+                }}
               />
-              <HomePageDescription text="The twist however is that both the instructor and the students must draw to ask or answer questions correctly!" />
+              <HomePageDescription
+                text={`Teachers can then:\n
+
+• Shuffle through responses to identify common misconceptions.\n
+• Annotate directly on submissions to highlight and correct mistakes.\n
+• Compile all responses into a single PDF and export it to their digital classroom platform for review or sharing.\n
+• The result is a more engaged classroom, faster feedback loops, and deeper learning.`}
+              />
             </div>
           </main>
         </div>
@@ -58,6 +81,7 @@ export default function Home() {
             <HomePageCrewMember
               name="George Postica"
               description="Description about George"
+              photoSrc={georgePFP.src}
             />
             <HomePageCrewMember
               name="Ethan Diep"
@@ -66,13 +90,17 @@ export default function Home() {
             <HomePageCrewMember
               name="Kaleel Maharaj"
               description="Description about Kaleel"
+              photoSrc={kaleelPFP.src}
             />
             <HomePageCrewMember
               name="Nhat Minh Phan"
               description="Description about Minh"
+              photoSrc={minhPFP.src}
             />
           </main>
         </div>
+
+        <HomePageGitHubLink />
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16 bg-[#73c5d4]">
           <p>
@@ -106,7 +134,21 @@ function HomePageIntroduction() {
     <div className="homepage-introduction">
       <img src={logoImage.src} alt="Logo of EchoDraw" id="brand-logo"></img>
       <p className="text-3xl lg:text-3xl !leading-tight mx-[100px] max-w-xl text-center">
-        <i>Drawings Draw Listeners</i>
+        <i>A real-time collaborative whiteboarding tool for classrooms!</i>
+      </p>
+    </div>
+  );
+}
+
+function HomePageGitHubLink() {
+  return (
+    <div className="homepage-github justify-center">
+      <p>So what're you waiting for?</p>
+      <p>
+        Check out our <a>GitHub repository</a>{" "}
+        <b>
+          <u>now!</u>
+        </b>
       </p>
     </div>
   );
@@ -121,12 +163,18 @@ const HomePageDescription: React.FC<HomePageDescriptionProps> = ({
   question = "",
   text,
 }) => {
+  const textToTextArray = (str: string) => {
+    return str.split("\n");
+  };
+
   return (
     <div className={`homepage-description`}>
       {question && question !== "" ? (
         <h2 className="font-medium text-xl mb-4">{question}</h2>
       ) : null}
-      <p className="mt-4">{text}</p>
+      {textToTextArray(text).map((str) => (
+        <p className="mt-4">{str}</p>
+      ))}
     </div>
   );
 };
@@ -134,17 +182,26 @@ const HomePageDescription: React.FC<HomePageDescriptionProps> = ({
 interface HomePageCrewMemberProps {
   name: string;
   description: string;
+  photoSrc?: string;
 }
 
 const HomePageCrewMember: React.FC<HomePageCrewMemberProps> = ({
   name,
   description,
+  photoSrc,
 }) => {
   return (
-    <div className="flex-1 flex flex-row gap-x-10 bg-[#34a8a2] px-8 py-5 rounded-[15px] homepage-description-section">
+    <div className="flex-1 flex flex-row gap-x-10 bg-[#34a8a2] px-8 pt-10 pb-5 rounded-[15px] homepage-description-section">
       <img
-        src={placeholderPFP.src}
-        style={{ width: "50%", height: "50%", borderRadius: "50%" }}
+        src={!photoSrc || photoSrc == "" ? placeholderPFP.src : photoSrc}
+        style={{
+          width: "25%",
+          height: "25%",
+          borderRadius: "50%",
+          borderColor: "black",
+          borderWidth: "2px",
+          objectFit: "contain",
+        }}
       />
       <div className={`homepage-description`}>
         <h2 className="font-medium text-xl mb-4">{name}</h2>
